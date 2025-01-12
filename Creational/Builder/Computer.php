4e944de77ecefd60e7b58e8568718d11ea699e42
Esteban-jv <ejv.developer@gmail.com>
2025-01-11 21:05:39 -0500

    Builder comments

--- Creational/Builder/Computer.php
+++ Creational/Builder/Computer.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * Patrón Builder
+ * Es un patron de diseño creacional que nos permite construir objetos complejos paso a paso
+ *
+ * El patrón nos permite producir distintos tipos y representaciones de un objeto
+ * empleando el mismo código de construcción.
+ *
+ * Es útil cuando necesitamos construir un objeto complejo con muchas partes y queremos
+ * que el proceso de construcción sea independiente de las partes que lo componen.
+ *
+ * https://refactoring.guru/design-patterns/builder
+ */
+
 class Computer {
     public $cpu;
     public $ram;
